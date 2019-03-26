b9ccd5c3cacea341efe333619ce39fac95883cb4
dhu0001 <16512386+dh0001@users.noreply.github.com>
2019-03-25 21:21:15 -0400

    base ideas

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    List<UnitManager> units;
+
+    // initialize
+    private void Start()
+    {
+        // create units, will be passed a list of units to create, somehow
+        List<string> passed = new List<string>();
+        passed.Add("axe");
+
+        units = new List<UnitManager>();
+
+        foreach (var p in passed)
+        {
+            units.Add(new UnitManager(p));
+        }
+
+
+        // start game loop
+
+
+    }
+}

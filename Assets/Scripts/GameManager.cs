d1f2ca312122c1b2a5865debd7ba6324e1af3c46
David Hu <16512386+dh0001@users.noreply.github.com>
2019-04-10 20:55:30 -0400

    resolved merge conflict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     List<UnitManager> units;
+    public Text text;
+    int buttonPressedCount = 0;
 
     // initialize
-    private void Start() 
+    private void Start()
     {
         // create units, will be passed a list of units to create, somehow
         List<string> passed = new List<string>();
@@ -16,14 +19,29 @@ public class GameManager : MonoBehaviour
         units = new List<UnitManager>();
 
         foreach (var p in passed)
+        {
             units.Add(new UnitManager(p));
+        }
+
+
+        // start game loop
+        StartCoroutine(Loop());
     }
 
+    private IEnumerator Loop()
+    {
+        // 
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                buttonPressedCount++;
+                text.text = buttonPressedCount.ToString();
 
-    void FixedUpdate() {
-        // check how many ticks have passed
-        // check the user's current input
-        // run through the ticks
+            }
+            yield return null;
+        }
         
     }
 }
--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -2,10 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class Unit
+{
+    public int MaxHealth;
+    public int VisionRange;
+
+    public Unit(int maxHealth, int visionRange)
+    {
+        MaxHealth = maxHealth;
+        VisionRange = visionRange;
+    }
+}
+
+public abstract class Action
+{
+    public abstract void Update();
+}
+
+public class MoveAction : Action
+{
+    public override void Update()
+    {
+    }
+}
+
+
+public class Modifiers
+{
+
+}
+
+public class ExampleHero : Unit
+{
+    ExampleHero(int visionRange, int maxHealth) : base(visionRange, maxHealth)
+    {
+    }
+}
+
+
+
 public class UnitManager
 {
+    private GameObject model;
+    private int health;
+    private Action action;
+    private List<Modifiers> modifiers;
+    private Unit unit;
+
+    // Alternative to using Resources.Load: https://unity3d.com/learn/tutorials/topics/best-practices/assetbundle-usage-patterns
+    // It is actually the recommended way of loading resources without a public reference to them, but a bit complex.
     public UnitManager(string s)
     {
-
+        model = Resources.Load("Units/Tank") as GameObject;
+        Object.Instantiate(model, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
     }
 }
